41e65317a1c1d0acdc0bd8ddddc1413f39fdd3fa
omgronny <68964770+omgronny@users.noreply.github.com>
2020-12-08 02:20:41 +0400

    Update samsuLab4Var17.py

--- samsuLab4Var17.py
+++ samsuLab4Var17.py
@@ -4,16 +4,16 @@ import math
 a=1.57
 b=2.38
 
-x=float(input("введите х "))
+x=float(input("введите х ")) # Вводим значение 
 
-if x<= -1:
+if x<= -1: # Если  х меньше -1
   
-  y = math.sqrt(abs(math.sin(a*x)))
-  print ("y= ", y)
+  y = math.sqrt(abs(math.sin(a*x))) # Формула
+  print ("y= ", y) # Выводим значение 
   
 else:
 
-  y=math.log(1 + (b*x)**2)
+  y=math.log(1 + (b*x)**2) # Иначе другая формула 
   print ("y= ", y)
 
 # task 2
@@ -23,19 +23,19 @@ import math
 a=2.7
 b=1.5
 
-x=float(input("введите х "))
+x=float(input("введите х ")) # Вводим значение 
 
-if x <= -1:
+if x <= -1: # Если  х максимум 3
 
-  y = 1/(1+x)**2
-  print ("y= ", y)
+  y = 1/(1+x)**2  # Формула
+  print ("y= ", y) 
 
-elif x > 1:
+elif x > 1: # Иначе если х больше 1
 
-  y = x**2 + math.cos(a)
+  y = x**2 + math.cos(a) # Формула
   print ("y= ", y)
 
 else:
-  y = math.sin(a*x + b)
+  y = math.sin(a*x + b) # в остальных случаях третья формула  
   print ("y= ", y)
   
