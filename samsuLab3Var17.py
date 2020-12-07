c13c027affbe05a7eebae14e8a3c728b75415aae
omgronny <68964770+omgronny@users.noreply.github.com>
2020-12-08 02:18:14 +0400

    Update samsuLab3Var17.py

--- samsuLab3Var17.py
+++ samsuLab3Var17.py
@@ -3,9 +3,9 @@ import math
 a=2.44
 b=1.39
 c=6.21
-x=float(input("введите х "))
+x=float(input("введите х ")) # Вводим число
 
-y = math.sqrt(x + math.exp(a*x)) * math.log((b*x**2 - 1)/(c*x**2 + 3))
+y = math.sqrt(x + math.exp(a*x)) * math.log((b*x**2 - 1)/(c*x**2 + 3)) # формула из варианта 
 
 print("y=",y)
-
+# выводим значение 
